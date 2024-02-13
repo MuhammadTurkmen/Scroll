c2d61b63a4b86c754446751d0d0902e683a98162
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-13 10:55:17 +0430

    init

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ date.innerHTML = new Date().getFullYear()
 const navToggle = document.querySelector('.nav-toggle')
 const linksContainer = document.querySelector('.links-container')
 const links = document.querySelector('.links')
+
+navToggle.addEventListener('click', function() {
+    
+})
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
