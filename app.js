def3b14b0a283bb3a55898caab21a8ab18a2a5dc
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-13 10:47:53 +0430

    init

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
 // pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
 // slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
+//offsetTop - A Number, representing the classtop position of the element, in pixels
 
 // ********** set date ************
-const date = document.getElementById('')
+const date = document.getElementById('date')
 
 // ********** close links ************
 
