fad981fddbbb548ff7bccd4171e68b8f8359dbdd
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-13 10:48:45 +0430

    init

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 // ********** set date ************
 const date = document.getElementById('date')
-
+date.innerHTML = new Date().getFullYear
 // ********** close links ************
 
 // ********** fixed navbar ************
