097e8262ef30022e9564e72810036c36df2a256f
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-14 18:52:10 +0430

    init

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ scrollLinks.forEach(function(link) {
         if(navHeight > 82) { 
             position = position + containerHeight  
         }
-        window.scrollTo({
+        window.scrollTo({ 
             left: 0,
             top: position
         })
