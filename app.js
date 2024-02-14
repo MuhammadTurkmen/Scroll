55faf1349547fce86d313baa392a3413c7c8b5ed
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-14 11:21:39 +0430

    init

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ navToggle.addEventListener('click', function() {
     }
 }) 
 
+const navbar = document.getElementById('')
 // ********** fixed navbar ************
 window.addEventListener('scroll', function() {  
  
